3745edd84e02440025ffb5dcc8c5fb75203c5203
Shishir-Salian <shishir.salian@msam.christuniversity.in>
2024-10-29 09:38:53 +0530

    AlphabetWarGame.java

--- AlphabetWarGame.java
+++ AlphabetWarGame.java
@@ -1,92 +1,69 @@
-import java.util.Scanner;
-
-public class AlphabetWarGame {
-    private char[] leftLetters;
-    private int[] leftStrengths;
-    private char[] rightLetters;
-    private int[] rightStrengths;
-
-    public AlphabetWarGame() {
-        leftLetters = new char[] {'w', 'p', 'b', 's'};
-        leftStrengths = new int[] {4, 3, 2, 1};
-
-        rightLetters = new char[] {'m', 'q', 'd', 'z'};
-        rightStrengths = new int[] {4, 3, 2, 1};
+public class AlphabetWar
+{
+    AlphabetWar()
+    {
+        
     }
-
-    public AlphabetWarGame(char[] leftLetters, int[] leftStrengths, char[] rightLetters, int[] rightStrengths) {
-        this.leftLetters = leftLetters;
-        this.leftStrengths = leftStrengths;
-        this.rightLetters = rightLetters;
-        this.rightStrengths = rightStrengths;
+    AlphabetWar(int Health)
+    {
+        Health1 = Health;
+        Health2 = Health;
     }
-
-    public String alphabetWar(String word) {
-        int leftTotal = 0;
-        int rightTotal = 0;
-
-        for (char c : word.toCharArray()) {
-            int index;
-            if ((index = indexOf(leftLetters, c)) != -1) {
-                leftTotal += leftStrengths[index];
-            } else if ((index = indexOf(rightLetters, c)) != -1) {
-                rightTotal += rightStrengths[index];
+    private int turn = 0;
+    private int damage = 0;
+    private final char[][] strength1 = {{'w', '4'}, {'p', '3'}, {'b', '2'}, {'s', '1'}};
+    private final char[][] strength2 = {{'m', '4'}, {'q', '3'}, {'d', '2'}, {'z', '1'}};
+    int Health1;
+    int Health2;
+    private void play(char warrior)
+    {
+        turn++;
+        if(turn%2==1)
+        {
+            for(int i = 0;i<strength1.length;i++)
+            {
+                if(strength1[i][0]==warrior)
+                {
+                    damage = Character.getNumericValue(strength1[i][1]);
+                }
             }
         }
-
-        return determineWinner(leftTotal, rightTotal);
-    }
-
-    public String alphabetWar(String leftWord, String rightWord) {
-        int leftTotal = 0;
-        int rightTotal = 0;
-
-        for (char c : leftWord.toCharArray()) {
-            int index;
-            if ((index = indexOf(leftLetters, c)) != -1) {
-                leftTotal += leftStrengths[index];
+        else if(turn%2==0)
+        {
+            for(int i = 0;i<strength2.length;i++)
+            {
+                if(strength2[i][0]==warrior)
+                {
+                    damage = damage - Character.getNumericValue(strength2[i][1]);
+                }
+            }
+            if(damage<0)
+            {
+                System.out.println("Player 2 Has damaged Player 1 by "+(damage*-1));
+                Health1 = Health1 - (damage*-1);
+                damage = 0;
+                System.out.println(Health1);
+            }
+            else if(damage>0)
+            {
+                System.out.println("Player 1 Has damaged Player 2 by "+(damage));
+                Health2 = Health2 - damage;
+                damage = 0;
+            }else{System.out.println("Error");}
+            if(Health1<=0)
+            {
+                System.out.println("Player 1 has reached 0 Health. Player 2 has won.");
+            }
+            else if(Health2<=0)
+            {
+                System.out.println("Player 2 has reached 0 Health. Player 1 has won.");
             }
         }
-
-        for (char c : rightWord.toCharArray()) {
-            int index;
-            if ((index = indexOf(rightLetters, c)) != -1) {
-                rightTotal += rightStrengths[index];
-            }
-        }
-
-        return determineWinner(leftTotal, rightTotal);
+        else{System.out.println("Error");}
     }
-
-    private String determineWinner(int leftTotal, int rightTotal) {
-        if (leftTotal > rightTotal) {
-            return "Left side wins!";
-        } else if (rightTotal > leftTotal) {
-            return "Right side wins!";
-        } else {
-            return "Let's fight again!";
-        }
-    }
-
-    private int indexOf(char[] array, char c) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == c) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        AlphabetWarGame game = new AlphabetWarGame();
-
-        System.out.println("Enter the word(s) for the Alphabet War:");
-        String input = scanner.nextLine();
-
-        String result = game.alphabetWar(input);
-        System.out.println(result);
-
-        scanner.close();
-    }
+	public static void main(String[] args) {
+		AlphabetWar obj = new AlphabetWar(3);
+		obj.play('w');
+		obj.play('z');
+	}
 }
